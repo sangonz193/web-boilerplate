b6926eacbb4cefa872b1ad96534c7d79532564d5
Santiago González <37671738+sangonz193@users.noreply.github.com>
2021-05-23 09:40:40 -0300

    fix routes config

--- src/routes/home/homeRoute.config.tsx
+++ src/routes/home/homeRoute.config.tsx
@@ -1,10 +1,11 @@
+import { appConfig } from "../../config/app.config"
 import { RouteConfig } from "../_utils/RouteConfig"
 import { Home } from "./Home"
 
 export const homeRouteConfig: RouteConfig = {
-	path: `/`,
+	path: appConfig.historyBasename || "/",
 	element: () => <Home />,
 	matchConfig: {
-		path: `/`,
+		path: appConfig.historyBasename || "/",
 	},
 }
