75dd7ec96ae1bd862f3ec6efc1ff6f9bad4d9b5f
Santiago González <37671738+sangonz193@users.noreply.github.com>
2021-05-23 10:01:37 -0300

    fix: :bug: fix routes config (#17) (#18)

--- src/modules/Navigation/Navigation.context.tsx
+++ src/modules/Navigation/Navigation.context.tsx
@@ -1,4 +1,4 @@
-import { createBrowserHistory, History } from "history"
+import { createHashHistory, History } from "history"
 import React from "react"
 
 import { useRefWithInitializer } from "../../hooks/useRefWithInitializer"
@@ -6,7 +6,7 @@ import { useRefWithInitializer } from "../../hooks/useRefWithInitializer"
 export const NavigationContext = React.createContext<History>(null as unknown as History)
 
 export const NavigationProvider: React.FC = ({ children }) => {
-	const history = useRefWithInitializer(() => createBrowserHistory()).current
+	const history = useRefWithInitializer(() => createHashHistory()).current
 
 	return <NavigationContext.Provider value={history}>{children}</NavigationContext.Provider>
 }
